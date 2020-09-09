6f49e95ff51ae1a5c74d6c6d7c1a4a2253852339
Joo Heredia <djjoo007uw@gmail.com>
2020-09-08 21:06:37 -0700

    add createConnection

---
+++ server.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root'
+    password: 'password'
+    database: 'employee_trackerDB'
+});
