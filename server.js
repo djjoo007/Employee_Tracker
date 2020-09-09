4d0f0196b52ca9cea90f1cdde823ecc883df8073
Joo Heredia <djjoo007uw@gmail.com>
2020-09-08 21:07:25 -0700

    corrected Connection info

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root'
-    password: 'password'
+    user: 'root',
+    password: 'password',
     database: 'employee_trackerDB'
 });
