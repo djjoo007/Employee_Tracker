2332c418a755bcd2c12d0abd29fd7927433e49e9
Joo Heredia <djjoo007uw@gmail.com>
2020-09-09 11:03:35 -0700

    modified seed sql

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -1,15 +1,20 @@
+-- Drops the programming_db if it already exists --
 DROP DATABASE IF EXISTS employee_trackerDB;
 
+-- Created the DB "employee-trackerDB" (only works on local connections)
 CREATE DATABASE employee_trackerDB;
 
+-- Use the DB "employee-trackerDB" for all the rest of the script
 USE employee_trackerDB;
 
+-- Create table for "department"
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id),
   name VARCHAR(30) NOT NULL
 );
 
+-- Create table for "role"
 CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id),
@@ -18,6 +23,7 @@ CREATE TABLE role(
   department_id INT(20) NOT NULL
 );
 
+-- Create table for "employee"
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id),
